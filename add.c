74740f00f1b5123c0db67be4938a0f0b6327efce
sudiptagihu <165810549+sudiptagihu@users.noreply.github.com>
2024-07-21 21:09:30 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a, b,c;
+    printf("Enter 1st No: ");
+    scanf("%d",&a);
+    printf("Enter 2nd No: ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("The sum is: %d",c);
+    return 0;
+}
